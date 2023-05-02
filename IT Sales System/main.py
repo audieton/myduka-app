07c7d44bb779f6b5943ce02614f618a3963406a6
audieton <127110031+audieton@users.noreply.github.com>
2023-05-02 15:16:00 +0300

    My duka

--- IT Sales System/main.py
+++ IT Sales System/main.py
@@ -7,8 +7,19 @@ def home():
 #Loading the html file
     return render_template('index.html')
 #This is the second route
-@app.route('/inventories')
+@app.route('/products')
 def products():
 #Loading the html file
-    return render_template ('products.html')
+#This is where we get data from products table
+#run pip install psycopg2
+   products[
+     (1,"omo",40,50,100)
+     (2,"bread",50,60,200)
+     (3,"milk",60,65,150)
+ ]
+ #This is where we load an html file
+   return render_template ('products.html',products=products)
 
+app.run()
+
+import psycopg2
